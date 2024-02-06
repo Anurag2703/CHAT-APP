3402ced9f60a171291b6b23406e25f716e1d272a
Anurag Panda <anuragpanda2704@gmail.com>
2024-02-06 18:03:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const express = require("express");
+const app =  express();
+
+const server = http.createServer(app);
+const port = 9000;
+
+server.listen(port, () => console.log(`Server started at PORT:  ${port}`));
